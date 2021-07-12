8e26f652edd0eca1b8c62a57af0db1e5c9b86d40
SangWoo Kang <kangsw9395@gmail.com>
2021-07-12 18:10:34 +0900

    Time: 2 ms (49.90%), Space: 42.8 MB (88.58%) - LeetHub

---
+++ counting-bits/counting-bits.java
@@ -0,0 +1,14 @@
+class Solution {
+  public int[] countBits(int n) {
+    int[] dp = new int[n + 1];
+    dp[0] = 0;
+    for (int i = 1; i <= n; i++) {
+      if (i == 1) {
+        dp[i] = 1;
+        continue;
+      }
+      dp[i] = dp[i >> 1] + dp[i & 1];
+    }
+    return dp;
+  }
+}
